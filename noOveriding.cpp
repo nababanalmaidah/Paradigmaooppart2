75575fbcee4290b71f151a93468a7c31796b6198
nababanalmaidah <nababanalmaidah@gmail.com>
2026-05-18 18:25:34 +0700

    menyelesaikan program noveriding

--- noOveriding.cpp
+++ noOveriding.cpp
@@ -7,3 +7,16 @@ public:
         cout << "Hallo saya Function dari base class";
     }
 };
+class derivedClass : public baseClass {
+public:
+    void perkenalan1() {
+        cout << "Hallo saya Function dari derived Class";
+    }
+};
+
+int main() {
+    derivedClass a;
+    a.perkenalan1();
+
+    return 0;
+}
