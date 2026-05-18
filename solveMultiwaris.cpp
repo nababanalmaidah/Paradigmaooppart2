56c7ad470795735854cf1b0d8d166e458b78febf
nababanalmaidah <nababanalmaidah@gmail.com>
2026-05-18 18:39:44 +0700

    menambahkan class

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -11,3 +11,31 @@ public:
         cout << "orang dibuat dengan umur " << umur << endl;
     }
 };
+
+class pekerja : public orang {
+public:
+    pekerja(int pUmur) :
+     orang(pUmur) 
+     {
+        cout << "pekerja dibuat\n" << endl;
+    }
+};
+
+class pelajar : virtual public orang {
+public:
+    pelajar(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pelajar dibuat\n" << endl;
+    }
+};
+
+class budi : public pekerja, public pelajar {
+public:
+    budi(int pUmur) :
+        pekerja(pUmur),
+        pelajar(pUmur)
+    {
+        cout << "Budi dibuat\n" << endl;
+    }
+};
