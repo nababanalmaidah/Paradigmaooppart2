daf2fb11f7a852111c7b39247994f0c2d8e6a04f
nababanalmaidah <nababanalmaidah@gmail.com>
2026-05-18 17:38:22 +0700

    membuat class remote lampu

---
+++ enskapsulasi.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+class remoteLampu {
+private:
+    string saklarNo[10];
+public:
+    void setSaklarNo(int i, string value) {
+        saklarNo[i] = value;
+    }
