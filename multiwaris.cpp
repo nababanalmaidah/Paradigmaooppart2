31347d4726afc7e5b285ed4c2f04bf76686e73b4
nababanalmaidah <nababanalmaidah@gmail.com>
2026-05-18 18:03:59 +0700

    update materi multiwaris

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -16,3 +16,15 @@ public:
     pekerja(int pUmur) :
         orang(pUmur)
     {
+        cout << "pekerja dibuat\n" << endl;
+    }
+};
+
+class pelajar : virtual public orang {
+public:
+    pelajar(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pelajar dibuat\n" << endl;
+    }
+};
