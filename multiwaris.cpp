e955f7f56af29b29824c3e6feb84afa406f26f87
nababanalmaidah <nababanalmaidah@gmail.com>
2026-05-18 18:01:27 +0700

    tambah class orang dan constructor

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -7,4 +7,12 @@ public:
     orang(int pUmur) :
         umur(pUmur)
     {
+        cout << "orang dibuat dengan umur " << umur << "\n" << endl;
     }
+};
+
+class pekerja : virtual public orang {
+public:
+    pekerja(int pUmur) :
+        orang(pUmur)
+    {
